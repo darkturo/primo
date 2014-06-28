b1ed50b58d6cb17568648e65c6e0e4a3f496101d
Arturo Escudero <darkturo@gmail.com>
2014-06-28 20:14:49 +0200

    Changed the way of finding the subsequent prime number
    I made a mistake using the table of maximal gaps as such, and thus I
    skipped some numbers. Now, I'm incrementing by two, to find the next
    one. In some cases, when there is a great jump, I will remain doing a
    bit of extra calculations, but for the time being I can't do much about
    it.

--- Iteration1/primo.pl
+++ Iteration1/primo.pl
@@ -47,15 +47,17 @@ sub nextFactor
 sub findNextPrime
 {
    my $currPrime = shift;
-   my @primeGapTable = (1, 2, 4, 6, 8, 14, 18, 20, 22, 34, 36, 44, 52, 
-                        72, 86, 96, 112, 114, 118, 132, 148, 154, 180, 
-                        210, 220);
    my $index = 0;
 
-   my $candidate = 0;
-   do {
-      $candidate = $currPrime + $primeGapTable[$index];
-   } while ( not isPrime($candidate) );
+   # Special case, where the gap is 1.
+   return 3 if ($currPrime == 2);
+
+   # Rest of the cases, the gap will be a multiple of 2. 
+   my $candidate = $currPrime + 2;
+   while ( not isPrime($candidate) )
+   {
+      $candidate += 2;
+   } 
 
    return $candidate;
 }
