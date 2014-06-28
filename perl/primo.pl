4b8aee5142036a578ed7b17292298e360b66a012
Arturo Escudero <darkturo@gmail.com>
2014-06-28 23:13:09 +0200

    New change in the algorithm, back to the usage of Firoozbakht conjecture.
    The idea is to use Firoozbakht to get a boundary number, and then select
    all the possible gaps that are less than that number; so that they can
    be tested.

--- perl/primo.pl
+++ perl/primo.pl
@@ -7,7 +7,7 @@
 use strict;
 use POSIX;
 
-my @primes = (2, 3);
+my @primes = (2);
 my $number = $primes[0]; 
 open FILE, ">primesEveryWhere.txt";
 for (my $i = 0; $i < 2000000; $i++)
@@ -40,29 +40,48 @@ sub isPrime
 sub findNextPrime
 {
    my $currPrime = shift;
-   my $index = 0;
 
-   # A list of gap jumps in according of their frequency. This list is accurate
+   # The gap between two consecutive prime numbers p and q, is always a
+   # multiple of 2, except for the case when p is 2, and q 3. 
+   # In the following table, I have the collection of gaps that will appear in
+   # the first 2M sequence. In principle, it is not more than count al the
+   # pairs, starting from 2; but in the end, the closer we are to 2M, some gaps
+   # are not present (at least in the first 2M numbers). So, with this list the
+   # idea is to avoid those.
    # for the first 2M numbers. 
-   my @primeGapTable = ( 6, 12, 2, 4, 10, 18, 8, 14, 24, 16, 30, 20, 22, 28, 26,
-                         36, 42, 34, 32, 40, 38, 48, 44, 54, 46, 50, 60, 52, 56,
-                         66, 58, 64, 62, 70, 72, 78, 68, 84, 74, 76, 90, 80, 82,
-                         88, 86, 96, 92, 94, 100, 102, 98, 108, 104, 114, 110,
-                         120, 112, 106, 126, 116, 118, 132, 122, 124, 138, 130,
-                         140, 136, 144, 134, 128, 148, 150, 142, 156, 154, 146,
-                         152, 210, 180, 170, 164, 1 );
+   my @primeGapTable = ( 1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28,
+                        30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56,
+                        58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84,
+                        86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 108, 
+                        110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 
+                        132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 
+                        154, 156, 164, 170, 180, 210, 220 );
 
+   # Remove the '1' from the table if p is greater than 2.
+   shift @primeGapTable if ( $currPrime > 2);
+
+   # Calculate a number N (using the cojecture of Firoozbakht) so that is
+   # greater than the gap betwen the prime (currPrime) and its subsequent
+   my $ln = log($currPrime) ;
+   my $gapBoundary = $ln**2 - $ln;
+
+   # The conjecture only works for the primes greater than the 4th prime (7)
+   $gapBoundary = 3 if ( $currPrime <= 7 );
+
+   # Select the list applicable gaps that are lower tan Firoozbakht number. 
+   my @gaps = grep { $_ < $gapBoundary } @primeGapTable;
+
+   my $index = 0;
    my $candidate = 0;
    do {
-      if ($index < @primeGapTable) 
+      if ($index < @gaps) 
       {
-         # jump using the more frequent gaps between numbers
-         $candidate = $currPrime + $primeGapTable[$index++];
+         $candidate = $currPrime + $gaps[$index++];
       }
       else
       {
-         # The number is out of reach, of the greedy strategy.
-         # Icrement steps of 2. 
+         # No prime was found using the selected gaps, continue testing for
+         # primality at increments of 2. 
          $candidate += 2;
       }
    } while ( not isPrime($candidate) ); 
