4a104cb1fc0aba19a061e75cb5fce3b1c00795b5
Arturo Escudero <darkturo@gmail.com>
2014-06-28 23:11:21 +0200

    Attempt to use the list of gap numbers according to their frequency.
    This attempt failed, since the selected gaps may lead to a prime number,
    but it may be that is not the subsequent :-(

--- perl/primo.pl
+++ perl/primo.pl
@@ -7,7 +7,7 @@
 use strict;
 use POSIX;
 
-my @primes = (2);
+my @primes = (2, 3);
 my $number = $primes[0]; 
 open FILE, ">primesEveryWhere.txt";
 for (my $i = 0; $i < 2000000; $i++)
@@ -42,15 +42,30 @@ sub findNextPrime
    my $currPrime = shift;
    my $index = 0;
 
-   # Special case, where the gap is 1.
-   return 3 if ($currPrime == 2);
+   # A list of gap jumps in according of their frequency. This list is accurate
+   # for the first 2M numbers. 
+   my @primeGapTable = ( 6, 12, 2, 4, 10, 18, 8, 14, 24, 16, 30, 20, 22, 28, 26,
+                         36, 42, 34, 32, 40, 38, 48, 44, 54, 46, 50, 60, 52, 56,
+                         66, 58, 64, 62, 70, 72, 78, 68, 84, 74, 76, 90, 80, 82,
+                         88, 86, 96, 92, 94, 100, 102, 98, 108, 104, 114, 110,
+                         120, 112, 106, 126, 116, 118, 132, 122, 124, 138, 130,
+                         140, 136, 144, 134, 128, 148, 150, 142, 156, 154, 146,
+                         152, 210, 180, 170, 164, 1 );
 
-   # Rest of the cases, the gap will be a multiple of 2. 
-   my $candidate = $currPrime + 2;
-   while ( not isPrime($candidate) )
-   {
-      $candidate += 2;
-   } 
+   my $candidate = 0;
+   do {
+      if ($index < @primeGapTable) 
+      {
+         # jump using the more frequent gaps between numbers
+         $candidate = $currPrime + $primeGapTable[$index++];
+      }
+      else
+      {
+         # The number is out of reach, of the greedy strategy.
+         # Icrement steps of 2. 
+         $candidate += 2;
+      }
+   } while ( not isPrime($candidate) ); 
 
    return $candidate;
 }
