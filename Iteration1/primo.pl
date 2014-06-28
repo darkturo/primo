d9f60e24da216434daf7cdf2700ddf262a3d4823
Arturo Escudero <darkturo@gmail.com>
2014-06-28 12:03:15 +0200

    Introduced idea of looking primes using the conjecture of Firoozbakht
    The idea is that instead of iterating over all the numbers and check for
    their primality, I will jump between numbers using the conjecture of
    Firoozbakht that helps to calculate a number that is equal or greater
    than the actual gap between a primer nmber and its subsequent.

--- Iteration1/primo.pl
+++ Iteration1/primo.pl
@@ -1,28 +1,31 @@
 #!/usr/bin/perl
 #
-# Simplistic implementation, using Eratosthenes Sieve
+# Find the first 2,000,000 prime numbers using Eratosthenes Sieve, and the
+# conjecture of Firoozbakht to find subsequent numbers (instead of just adding
+# one to each candidate).
 # 
 # darkturo 2014.
 use strict;
 use POSIX;
 
-my $candidate = 2; 
-my $numbersFound = 0;
+my $number = 2; 
 open FILE, ">primesEveryWhere.txt";
-while ($numbersFound != 2000000) 
+for (my $i = 0; $i < 2000000; $i++)
 {
-   if ( isPrime( $candidate ) )
-   {
-      print FILE "$candidate\n";
-      $numbersFound++;
-   }
-   $candidate++;
+   print FILE "$number\n";
+   $number = findNextPrime($candidate);
 }
 close(FILE);
 
+###################################
 sub isPrime
 {
    my $n = shift;
+
+   if ( ( $n % 2 == 0 ) or ( $n % 3 == 0 ) or ( $n % 5 == 0 ) )
+   {
+      return 0 unless ( $n == 2 or $n == 3 or $n == 5 );
+   }
 
    my $squareOfN = sqrt($n);
    for (my $factor = floor($squareOfN); $factor > 1; $factor--)
@@ -33,4 +36,29 @@ sub isPrime
       }
    }
    return 1;
+}
+
+my @preCached = (2, 3, 5, 7, 11)
+sub findNextPrime
+{
+   my $currPrime = shift;
+
+   if (@preCached)
+   {
+      # The conjecture works for p_n where n is greater than 4, that's why I'll
+      # start with 11. 
+      return shift @preCached;
+   }
+
+   # Use the conjecture of Firoozbakht to find the gap boundary, between the
+   # current prime and the next prime.
+   my $lnCurrCand = log($currCandidate) ;
+   my $gapBoundary = $lnCurrCand**2 - $lnCurrCand;
+   
+   my $candidate = $currPrime + $gapBoundary;
+   while ( not isPrime($candidate) )
+   {
+      $candidate--;
+   }
+   return $candidate;
 }
