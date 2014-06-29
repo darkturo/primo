a20e2e2da7283f1c4ae36afbd694a88fad0295f8
Arturo Escudero <darkturo@gmail.com>
2014-06-29 03:18:21 +0200

    Removed all the intermediate tables.
    I have removed all the tables I used, in order to make faster the
    program. It seems that the only option, in order to make it faster is to
    think this from a different angle, and try to use parallel programming.

--- perl/primo.pl
+++ perl/primo.pl
@@ -7,25 +7,8 @@
 use strict;
 use POSIX;
 
-# The gap between two consecutive prime numbers p and q, is always a
-# multiple of 2, except for the case when p is 2, and q 3. 
-# In the following table, I have the collection of gaps that will appear in
-# the first 2M sequence. In principle, it is not more than count al the
-# pairs, starting from 2; but in the end, the closer we are to 2M, some gaps
-# are not present (at least in the first 2M numbers). So, with this list the
-# idea is to avoid some calculations.
-my @primeGapTable = ( 1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28,
-                     30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56,
-                     58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84,
-                     86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 108, 
-                     110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 
-                     132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 
-                     154, 156, 164, 170, 180, 210, 220 );
-
-my @primes = (2);
-
 # Program
-my $number = $primes[0]; 
+my $number = 2;
 open FILE, ">primesEveryWhere.txt";
 for (my $i = 0; $i < 2000000; $i++)
 {
@@ -38,39 +21,22 @@ close(FILE);
 sub isPrime
 {
    my $n = shift;
+
+   return 1 if ( $n == 2 or $n == 3 or $n == 5 ); 
+   return 0 if ( $n % 2 == 0 or $n % 3 == 0 or $n % 5 == 0);
    
-   my $factor = floor( sqrt($n) );
-   for (; $factor > 1; $factor = previousFactor($factor))
+   my $pivot = floor( sqrt($n) );
+   $pivot-- if ($pivot > 2 and $pivot % 2 == 0);
+
+   for (; $pivot > 5; $pivot -= 2)
    {
-      if ($n % $factor == 0)
+      if ($n % $pivot == 0)
       {
          return 0;
       }
    }
 
-   push @primes, $n;
    return 1;
-}
-
-sub previousFactor 
-{
-   my $currentFactor = shift;
-
-   # Special cases
-   return 0 if ( $currentFactor == 2 );
-   return 2 if ( $currentFactor == 3 );
-
-   my $factor = $currentFactor;
-   if ($currentFactor % 2 == 0)
-   {
-      $factor --;
-   }
-   else
-   {
-      $factor -= 2;
-   }
-   
-   return $factor;
 }
 
 sub findNextPrime
@@ -78,33 +44,13 @@ sub findNextPrime
    my $currPrime = shift;
 
    # Special case.
-   return 3 if ( $currPrime <= 2);
+   return 3 if ( $currPrime == 2);
 
-   # Calculate a number N (using the cojecture of Firoozbakht) so that is
-   # greater than the gap betwen the prime (currPrime) and its subsequent
-   my $ln = log($currPrime) ;
-   my $gapBoundary = $ln**2 - $ln;
-
-   # The conjecture only works for the primes greater than the 4th prime (7)
-   $gapBoundary = 5 if ( $currPrime <= 7 );
-
-
-   my $index = 0;
-   my $candidate = 0;
-   # Select the list applicable gaps that are lower tan Firoozbakht number. 
-   do {
-      if ($primeGapTable[$index] < $gapBoundary) 
-      {
-         if ($index < @primeGapTable)
-         {
-            $candidate = $currPrime + $primeGapTable[$index++];
-         }
-         else 
-         {
-            $candidate += 2;
-         }
-      }
-   } while ( not isPrime($candidate) ); 
-
-   return $candidate;
+   my $nextPrime = $currPrime;
+   do
+   {
+      $nextPrime += 2;
+   } while ( not isPrime($nextPrime) ); 
+   
+   return $nextPrime;
 }
