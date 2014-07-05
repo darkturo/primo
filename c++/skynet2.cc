8a6f70ea176af895b3bf13fa8e679cb59e56a3fd
Arturo Escudero <darkturo@gmail.com>
2014-07-05 13:26:36 +0200

    Start again to code based on skynet 1
    This time I'm having in mind that there is a limit to template
    recursion, and also that the access to the recursive template as I was
    dong have an expensive cost :-(
    So I'm using a bitset now, and the idea is to do a precalculation in
    compile time of the multiples.

---
+++ c++/skynet2.cc
@@ -0,0 +1,262 @@
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <limits>
+
+#define MID(a, b) ((a+b)/2)
+#define POW(a) (a*a)
+
+template<long long int res, long long int l = 1, long long int r = res>
+struct SQRT;
+
+template<long long int res, long long int r>
+struct SQRT<res, r, r> {
+   static const long long int value = r;
+};
+
+template <long long int res, long long int l, long long int r>
+struct SQRT {
+   static const long long int value = SQRT<res, 
+                                (POW(MID(r, l)) >= res ?  l : MID(r, l)+1),
+                                (POW(MID(r, l)) >= res ? MID(r, l) : r)>::value;
+};
+
+template <bool Condition, typename TrueResult, typename FalseResult>
+struct if_;
+ 
+template <typename TrueResult, typename FalseResult>
+struct if_<true, TrueResult, FalseResult>
+{
+   typedef TrueResult result;
+};
+ 
+template <typename TrueResult, typename FalseResult>
+struct if_<false, TrueResult, FalseResult>
+{
+   typedef FalseResult result;
+};
+/////////////////
+ 
+class end_of_sieve
+{
+   public:
+   bool isComposite(int n)
+   {
+      return true;
+   }
+
+   int print()
+   {
+      /* done! */
+      return 0;
+   }
+};
+
+template <int base, int max, typename listOfNaturals>
+class sieve_iterate_rest
+{
+   enum { value = base };
+   
+   public:
+   bool isComposite(int n)
+   {
+      listOfNaturals N;
+      return N.isComposite(n);
+   }
+
+   int print()
+   {
+      typename if_< ((base + 2) <= max + 2),
+                  sieve_iterate_rest<base + 2, max, listOfNaturals>, 
+                  end_of_sieve>::result sieve;
+      int count = 0;
+
+      if ( not sieve.isComposite(base) ) 
+      {
+         std::cout << value << std::endl;
+         count ++;
+      }
+      return count + sieve.print();
+   }
+};
+
+class end_of_list
+{
+   public:
+   bool has(int n)
+   {
+      return false;
+   }
+};
+
+template <int pivot, int base, int max>
+class sieve_mark_multiples_iter
+{
+   enum { value = pivot };
+   
+   typename if_< (pivot + 2 * base) <= max,
+              sieve_mark_multiples_iter<pivot + 2 * base, base, max>, 
+              end_of_list >::result multiplesOfBase;
+
+   public:
+   bool has(int n)
+   {
+      //std::cout << "Checking " << n << " (value = " << value << ", base = " << base << ")." << std::endl;
+      if ( n == value )
+         return true;
+      else
+         return multiplesOfBase.has(n); 
+   }
+};
+
+template <int base, int max, typename listOfNaturals>
+class sieve_mark_multiples 
+{
+   enum { value = base };
+   
+   public:
+   bool isComposite(int n)
+   {
+      typename if_< (base + 2 * base) <= max,
+                  sieve_mark_multiples_iter<base + 2 * base, base, max>, 
+                  end_of_list >::result multiplesOfBase;
+      listOfNaturals naturals;
+
+      if ( n == value )
+         return false;
+      else 
+         if ( multiplesOfBase.has(n) )
+            return true;
+         else
+            return naturals.isComposite(n);
+   }
+};
+
+template <int max, typename listOfNaturals>
+class sieve_mark_multiples<2, max, listOfNaturals>
+{
+   public:
+   bool isComposite(int n)
+   {
+      return (n % 2 == 0);
+   }
+};
+
+template <int base, int sqrt, int max, typename listOfNaturals>
+class sieve_explore
+{
+   enum { value = base };
+   typename if_< base <= sqrt, 
+               sieve_explore<base + 2, sqrt, max, sieve_mark_multiples<base, max, listOfNaturals> >,
+               sieve_iterate_rest<base + 2, max, listOfNaturals> >::result sieve;
+
+   public:
+   int print()
+   {
+      listOfNaturals N;
+      int count = 0;
+
+      if ( not N.isComposite(base) )
+      {
+         std::cout << value << std::endl;
+         count++;
+      }
+      return count + sieve.print();
+   }
+};
+
+template <int sqrt, int max, typename listOfNaturals>
+class sieve_explore<2, sqrt, max, listOfNaturals>
+{
+   enum { value = 2 };
+   typename if_< value <= sqrt, 
+               sieve_explore<value + 1, sqrt, max, sieve_mark_multiples<2, max, listOfNaturals> >,
+               sieve_iterate_rest<value + 1, max, listOfNaturals> >::result sieve;
+   public:
+   int print()
+   {
+      std::cout << value << std::endl;
+      int count = 1;
+      if (max > value) 
+         count += sieve.print();
+      return count;
+   }
+};
+
+template <int n>
+class erasthostenes_t {
+public:
+   int sieve() 
+   {
+      if (n > 1)
+      {
+         sieve_explore<2, SQRT<n>::value, n, end_of_sieve> sieve;
+         return sieve.print();
+      }
+      return 0;
+   }
+};
+
+//////
+template <int MaxNumber>
+class ErasthostenesSieve
+{
+   std::bitset<MaxNumber> listOfNaturals;
+   std::fstream output;
+   char biggerbuffer[512000 * 8];
+
+   public:
+   ErasthostenesSieve()
+   {
+      listOfNaturals.set();
+      listOfNaturals.set(0, false); // 1 is not prime
+      output.open("primesEveryWhere.txt", std::fstream::out);
+      output.rdbuf()->pubsetbuf(biggerbuffer, 512000);
+   }
+
+   ~ErasthostenesSieve()
+   {
+      output.close();
+   }
+
+   void applyTheSieve()
+   {
+
+   }
+
+   int printPrimes()
+   {
+      int counter = 0;
+      for (int i = 0; i < MaxNumber; i++)
+      {
+         if (listOfNaturals[i])
+         {
+            output << i << std::endl;
+            counter ++;
+         }
+      }
+      return counter;
+   }
+};
+
+int main(int argc, char ** argv)
+{
+   const int MaxNumber = 32288611;
+   time_t t1, t2;
+
+
+   time(&t1);
+
+   ErasthostenesSieve<MaxNumber> siever;
+   siever.applyTheSieve();
+   int printedPrimes = siever.printPrimes();
+
+   time(&t2);
+
+   // Summary
+   std::cout.precision(std::numeric_limits<double>::digits10);
+   std::cerr << "Used " << std::fixed << difftime(t2, t1) 
+             << " seconds to calculate " << printedPrimes
+             << " primes." << std::endl;
+}
