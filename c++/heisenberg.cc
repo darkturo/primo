c9305b9daee052049dccfc2b9ff7ebc6c0d7c3cc
Arturo Escudero <arturo.escudero@ericsson.com>
2014-07-07 01:13:45 +0200

    Fix the mystery, in the lmwps we have an old version of boost ...

--- c++/heisenberg.cc
+++ c++/heisenberg.cc
@@ -52,7 +52,7 @@ class ErasthostenesSieve
       output_fd = open("primesEveryWhere.txt", O_RDWR | O_CREAT | O_TRUNC, (mode_t)0664);
       lseek(output_fd, FILESIZE-1, SEEK_SET);
       write(output_fd, "", 1);
-      p_map = (char*)mmap(0, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, output_fd, 0);
+      p_map = (char*)mmap(0, FILESIZE, PROT_WRITE, MAP_SHARED, output_fd, 0);
       p_input = p_map;
 #endif
    }
@@ -64,7 +64,8 @@ class ErasthostenesSieve
          output.write(buffer, p_input - buffer);  // flush
       output.close();
 #else
-      munmap(p_map, p_input - p_map);
+      msync(p_map, FILESIZE, MS_ASYNC);
+      munmap(p_map, FILESIZE);
       close(output_fd);
 #endif
    }
@@ -119,6 +120,7 @@ class ErasthostenesSieve
    inline void print(int number)
    {
       boost::spirit::karma::generate(p_input, boost::spirit::int_, number);
+      p_input += strlen(p_input);
       *p_input++ = '\n';
       counter ++;
    }
